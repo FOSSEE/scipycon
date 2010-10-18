51598b236c635b892ca80620a4efdd2f70e96f04
Puneeth Chaganti <punchagan@fossee.in>
2010-10-18 20:30:01 +0530

    Fixed the bug with sending the mail content post registration.

--- project/scipycon/registration/utils.py
+++ project/scipycon/registration/utils.py
@@ -8,26 +8,31 @@ def send_confirmation(registrant, event):
     message.from_email = u'admin@scipy.in'
     message.to = [registrant.email]
 
-    username = registrant.username
-    all = {'name': registrant.get_full_name(),
-           'username': username,
-           'event_name': event.get_full_name(),
-           'event_scope': event.scope,
-           }
+    details = {'name': registrant.get_full_name(),
+               'username': registrant.username,
+               'password': registrant.password,
+               'event_name': event.get_full_name(),
+               'event_scope': event.scope,
+            }
+
+    confirmation_newuser = """Dear %(name)s,
+
+Thank you, for registering for %(event_name)s!
+
+You may log in to the %(event_name)s website at
+http://scipy.in/%(event_scope)s/login using the username -
+%(username)s and the password - %(password)s.
+
+Looking forward to meet you at %(event_name)s.
+
+Regards,
+SciPy.in Team
+
+If you lose your password, visit: http://scipy.in/password-reset
+"""
+
+    message.body = confirmation_newuser %(details)
 
     message.send()
 
-confirmation_newuser = """Dear %(name)s,
 
-Welcome to %(event_name)s. You have registered for %(event_name)s with the
-username %(username)s. You may log in to the %(event_name)s website at
-http://scipy.in/%(event_scope)s/login using the given username
-
-Thank you for your registration! Looking forward to meet you at %(event_name)s.
-
-Regards,
-The SciPy.in Team
-
-If you lose your password to the website please visit:
-http://scipy.in/password-reset
-"""
