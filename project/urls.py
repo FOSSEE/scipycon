442c0d2f8b7f99cfdab06ea4ae5583e5d9eca409
Anoop Jacob Thomas <anoop@fossee.in>
2010-10-29 19:59:03 +0530

    added organizers page.

--- project/urls.py
+++ project/urls.py
@@ -119,6 +119,9 @@ urlpatterns += patterns('',
     url(r'^%s/about/fees/$' % (SCOPE_ARG_PATTERN),
         direct_to_template, {"template": "about/fees.html"},
         name='scipycon_fees'),
+    url(r'^%s/organizers/$' % (SCOPE_ARG_PATTERN),
+        direct_to_template, {"template": "about/organizers.html"},
+        name='scipycon_organizers'),
 
     )
 
